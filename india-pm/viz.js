6c7d2c4205dd4993b05dcebf3699a42878b12e89
Puneeth Chaganti <punchagan@muse-amuse.in>
2017-09-20 21:21:56 +0530

    Add tooltip on hover

--- india-pm/viz.js
+++ india-pm/viz.js
@@ -29,9 +29,25 @@ var tick_format = {
     .range(colors)
     .domain(parties);
 
+var format_pm_term = function(pm){
+    var html = "";
+    html += "<strong>" + pm.label + "</strong><br/>";
+    html += "<strong>Party:</strong> " + pm.party +"<br/>";
+    html += "<strong>Term Start:</strong> " + new Date(pm.starting_time).toDateString() +"<br/>";
+    html += "<strong>Term End:</strong> " + new Date(pm.ending_time).toDateString() +"<br/>";
+    return html;
+};
+
+var add_tooltip = function(){
+    d3.select("#chart").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+};
 
 var process_data = function(error, data){
     if (error) throw error;
+
+    add_tooltip();
 
     var chart = d3.timelines()
         .stack()
@@ -39,7 +55,23 @@ var process_data = function(error, data){
         .itemHeight(30)
         .colors(colorScale)
         .colorProperty('party')
-        .rowSeparators('lightgrey');
+        .rowSeparators('lightgrey')
+        .mouseover(function(d) {
+            var tooltip = d3.select('.tooltip');
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            console.log(d);
+            tooltip.html(format_pm_term(d))
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .mouseout(function(d) {
+            var tooltip = d3.select('.tooltip');
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
 
     var svg = d3.select("#chart").append("svg").attr("width", window.innerWidth - 30)
         .datum(data).call(chart);

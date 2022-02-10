f0f11efebbced74eaf971f030d45775a98e747b0
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-02-11 00:24:08 +0530

    Format script with black

--- blog/process_data.py
+++ blog/process_data.py
@@ -9,14 +9,11 @@ import maya
 from nltk.corpus import stopwords
 import yaml
 
-CUSTOM = [
-    'https', 'http', 'www', 'com'
-]
-STOP_WORDS = list(stopwords.words('english')) + list(punctuation) + CUSTOM
+CUSTOM = ["https", "http", "www", "com"]
+STOP_WORDS = list(stopwords.words("english")) + list(punctuation) + CUSTOM
 
 
 class MLStripper(HTMLParser):
-
     def __init__(self):
         super(MLStripper, self).__init__()
         self.reset()
@@ -28,7 +25,7 @@ class MLStripper(HTMLParser):
         self.fed.append(d)
 
     def get_data(self):
-        return ''.join(self.fed)
+        return "".join(self.fed)
 
 
 def strip_tags(html):
@@ -42,24 +39,22 @@ def parse_post(post):
         lines = []
         for line in f:
             line = line.strip()
-            if line == '---':
+            if line == "---":
                 if len(lines) > 0:
                     break
+
             else:
                 lines.append(line)
         post_content = f.read()
-        metadata = yaml.load('\n'.join(lines))
-
-    date = metadata['date']
+        metadata = yaml.load("\n".join(lines))
+    date = metadata["date"]
     if isinstance(date, str):
-        metadata['date'] = maya.parse(date).datetime()
+        metadata["date"] = maya.parse(date).datetime()
     else:
-        metadata['date'] = maya.to_utc_offset_aware(date)
-
-    metadata.setdefault('tags', [])
-    metadata.setdefault('content', post_content)
-    metadata.setdefault('word_counts',
-                        non_stop_words(strip_tags(post_content)))
+        metadata["date"] = maya.to_utc_offset_aware(date)
+    metadata.setdefault("tags", [])
+    metadata.setdefault("content", post_content)
+    metadata.setdefault("word_counts", non_stop_words(strip_tags(post_content)))
     return metadata
 
 
@@ -72,44 +67,34 @@ def non_stop_words(text):
     for key in list(counts.keys()):
         if len(key) < 3:
             counts.pop(key)
-
     return counts
 
 
 def word_counts_by_year(posts, filter_=None):
     posts_by_year = {}
     for post in posts:
-        posts_by_year.setdefault(post['date'].year, []).append(post)
-
+        posts_by_year.setdefault(post["date"].year, []).append(post)
     counts = {}
     for year, year_posts in posts_by_year.items():
         counter = Counter()
         for post in year_posts:
-            if filter_ in {'TFIDF'}:
-                counter.update(set(post['word_counts'].keys()))
+            if filter_ in {"TFIDF"}:
+                counter.update(set(post["word_counts"].keys()))
             else:
-                counter.update(post['word_counts'])
-
-        if filter_ == 'TOP':
+                counter.update(post["word_counts"])
+        if filter_ == "TOP":
             items = counter.most_common(50)
         else:
             items = counter.items()
-
-        words = [
-            {"text": word, "count": count}
-            for word, count in items
-        ]
-
+        words = [{"text": word, "count": count} for word, count in items]
         counts[year] = words
-
-    if filter_ == 'TFIDF':
+    if filter_ == "TFIDF":
         counts = tf_idf(counts)
-
     return counts
 
 
 def tf(word, wordlist):
-    return word['count'] / len(wordlist)
+    return word["count"] / len(wordlist)
 
 
 def n_containing(word, word_counts):
@@ -125,32 +110,26 @@ def tf_idf(counts):
     for year, wordlist in counts.items():
         wordlist_ = [dict(word) for word in wordlist]
         for word in wordlist_:
-            word['count'] = int(tf(word, wordlist) * idf(word, counts) * 1000)
-
+            word["count"] = int(tf(word, wordlist) * idf(word, counts) * 1000)
         tf_idf_counts[year] = wordlist_
     return tf_idf_counts
 
 
 def create_word_count_json(posts):
     counts = word_counts_by_year(posts)
-    with open('data/counts.json', 'w') as f:
+    with open("data/counts.json", "w") as f:
+        json.dump(counts, f, indent=2)
+    counts = word_counts_by_year(posts, "TOP")
+    with open("data/counts-top.json", "w") as f:
+        json.dump(counts, f, indent=2)
+    counts = word_counts_by_year(posts, "TFIDF")
+    with open("data/counts-tfidf.json", "w") as f:
         json.dump(counts, f, indent=2)
 
-    counts = word_counts_by_year(posts, 'TOP')
-    with open('data/counts-top.json', 'w') as f:
-        json.dump(counts, f, indent=2)
 
-    counts = word_counts_by_year(posts, 'TFIDF')
-    with open('data/counts-tfidf.json', 'w') as f:
-        json.dump(counts, f, indent=2)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     BLOG_DIR = "/home/punchagan/software/my-repos/muse-amuse.in/content/blog"
-    posts = [
-        parse_post('{}/{}'.format(BLOG_DIR, filename))
-        for filename in os.listdir(BLOG_DIR)
-    ]
-    SKIP_TITLES = ('Bookmarks [', 'What I liked')
-    posts = [p for p in posts if not str(p['title']).startswith(SKIP_TITLES)]
-    create_word_count_json(posts)
+    posts = [parse_post("{}/{}".format(BLOG_DIR, filename)) for filename in os.listdir(BLOG_DIR)]
+    SKIP_TITLES = ("Bookmarks [", "What I liked")
+    posts = [p for p in posts if not str(p["title"]).startswith(SKIP_TITLES)]
+# create_word_count_json(posts)
